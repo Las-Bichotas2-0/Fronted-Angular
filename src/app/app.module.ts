9d9c2b8e8e51e8cb458c0286873680ed3d05584c
David-lp-ops <54964742+David-lp-ops@users.noreply.github.com>
2021-06-10 17:11:31 -0500

    fix:Create Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BannerComponent } from './banner/banner.component';
+import {NavbarComponent} from "./navbar/navbar.component";
 
 
 
@@ -15,9 +16,14 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { CallToRegisterComponent } from './call-to-register/call-to-register.component';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     BannerComponent,
     CallToRegisterComponent
   ],
@@ -28,7 +34,10 @@ import {MatListModule} from '@angular/material/list';
     MatGridListModule,
     MatButtonModule,
     MatCardModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
