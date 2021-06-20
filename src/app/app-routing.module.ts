fa91d0f1687beb4b6463de9711001ff8b1297e03
David-lp-ops <54964742+David-lp-ops@users.noreply.github.com>
2021-06-19 19:24:13 -0500

    fix:FixNavbar_Button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {MainComponent} from "./main/main.component";
 const routes: Routes = [
-  { path: '', component: HomeComponent},
   {path:'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
