37fa2cb12992a69acd5baa75b11688567c76cf2e
Sebastian Toulier <sebaas14toulier@gmail.com>
2021-06-27 21:18:35 -0500

    feat: add http methos to get

--- src/app/core/services/session-api.service.ts
+++ src/app/core/services/session-api.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {throwError} from "rxjs";
+import {SessionOutput} from "../models/outputs/session-output"
+import {SubscriptionInput} from "../models/inputs/subscription-input";
+import {catchError, retry} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +24,16 @@ export class SessionApiService {
       console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
     }
     return throwError('Something happened with request, please try again later.').toPromise();
+  }
+
+  //? TODO:  do i have to replace for SessionInput?
+  addSession(data: any): Promise<SessionOutput>{
+    return this.http.post<SessionOutput>(this.basePath, JSON.stringify(data), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError)).toPromise();
+  }
+
+  getAllSessions(): Promise<SessionOutput>{
+    return this.http.get<SessionOutput>(this.basePath)
+      .pipe(retry(2), catchError(this.handleError)).toPromise();
   }
 }
