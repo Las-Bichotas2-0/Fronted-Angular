4039ca58240ef3d55796d365630e83819b70a46d
Sebastian Toulier <sebaas14toulier@gmail.com>
2021-06-28 11:12:31 -0500

    feat: push languages to a list

--- src/app/components/create-session/create-session.component.ts
+++ src/app/components/create-session/create-session.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {TopicsApiService} from "../../core/services/topics-api.service";
+import {LanguagesApiService} from "../../core/services/languages-api.service";
 
 @Component({
   selector: 'app-create-session',
@@ -10,16 +11,18 @@ import {TopicsApiService} from "../../core/services/topics-api.service";
 export class CreateSessionComponent implements OnInit {
 
   selectedTopic: string | undefined;
+  selectedLanguage: string | undefined;
   topics: any[] = [];
   languages: any[] = [];
 
-  constructor(private router: Router, private topicsApi: TopicsApiService) {
+  constructor(private router: Router, private topicsApi: TopicsApiService, private languageApi: LanguagesApiService) {
     this.topics = [];
     this.languages = [];
   }
 
   ngOnInit(): void {
     this.getAllTopics();
+    this.getAllLanguages();
   }
 
 
@@ -32,6 +35,17 @@ export class CreateSessionComponent implements OnInit {
         console.log(this.topics);
       })
       .catch((error)=>console.log(error))
+  }
+
+  getAllLanguages(): void{
+    this.languageApi.getAllLanguages()
+      .then((response: any) =>{
+        for(let i = 0; i < response.content.length; i++){
+          this.languages.push(response.content[i]);
+        }
+        console.log(this.languages);
+      })
+      .catch((e) => console.log(e));
   }
 
 
